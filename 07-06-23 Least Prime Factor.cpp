672a3d71352c52ea498b3a558460ed699272e78a
Akash Verma <75831162+akashazad-dot@users.noreply.github.com>
2023-06-07 23:23:26 +0530

    07-06-23 Least Prime Factor.cpp

---
+++ 07-06-23 Least Prime Factor.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int prime(int n){
+           if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        
+        for(int i=2;i<=n;i++){
+            if(n%i==0){
+                return i;
+            }
+        }
+    }
+    vector<int> leastPrimeFactor(int n) {
+        vector<int>ans;
+        for(int i=0;i<=n;i++){
+            ans.push_back(prime(i));
+        }
+        
+        return ans;
+  
