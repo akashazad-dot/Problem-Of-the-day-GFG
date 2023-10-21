4ffc1ff426ffd66b5583d27138ee0c925dff77f6
Akash Verma <75831162+akashazad-dot@users.noreply.github.com>
2023-10-21 13:18:37 +0530

    18-07-23 Longest Repeating Subsequence.cpp

---
+++ 18-07-23 Longest Repeating Subsequence.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+    public:
+        int LongestRepeatingSubsequence(string str){
+            int l=str.length();
+            vector<int> prev(l+1, 0);
+            vector<int> curr(l+1);
+            curr[0]=0;
+            for(int i=0; i<l; i++) {
+                for(int j=0; j<l; j++) {
+                    if(i!=j && str[i]==str[j]) {
+                        curr[j+1]=1+prev[j];
+                    }
+                    else {
+                        curr[j+1]=max(curr[j], prev[j+1]);
+                    }
+                }
+                prev=curr;
+            }
+            return prev[l];
+        }
+
+};
