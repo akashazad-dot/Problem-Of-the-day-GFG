fb13bac6f884ce0b8a34c280620df51d4a1ed3f1
Akash Verma <75831162+akashazad-dot@users.noreply.github.com>
2023-10-23 23:11:45 +0530

    23-10-23 Maximum sum increasing subsequence.cpp

---
+++ 23-10-23 Maximum sum increasing subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+int maxSumIS(int arr[], int n)  
+{  
+    // Your code goes here
+    int dp[n];
+    for(int i = 0; i < n; i++){
+        dp[i] = arr[i];
+        for(int j = 0; j < i; j++){
+            if(arr[j] < arr[i])
+                dp[i] = max(dp[i], arr[i]+dp[j]);
+        }
+    }
+    return *max_element(dp, dp+n);
+}  
+};
