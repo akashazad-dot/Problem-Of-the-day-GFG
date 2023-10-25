0caf09825364a9cab364f2da038a8de5b2374e93
Akash Verma <75831162+akashazad-dot@users.noreply.github.com>
2023-10-25 22:26:24 +0530

    17-08-23 Next Smallest Palindrome.cpp

---
+++ 17-08-23 Next Smallest Palindrome.cpp
@@ -0,0 +1,98 @@
+//User function template for C++
+class Solution{
+public:
+ vector<int> generateNextPalindrome(int num[], int n)
+    {
+        int change = 0;
+        bool flag = false, f1 = false;
+        vector<int> ans(n);
+        int po = INT_MAX , ne = INT_MAX;
+        for (int i = 0; i < n / 2 + n % 2; i++)
+        {
+            if (num[i] != num[n - i - 1])
+            {
+                change = num[i] - num[n - i - 1];
+                if (change > 0)
+                    po = n - 1 - i;
+                if (change < 0)
+                    ne = n - i - 1;
+                f1 = true;
+                ans[i] = num[i];
+                ans[n - 1 - i] = num[i];
+            }
+            else
+            {
+                ans[i] = num[i];
+                ans[n - 1 - i] = num[n - 1 - i];
+            }
+        }
+        if (po < ne)
+        {
+            return ans;
+        }
+        else
+        {
+            if ((n & 1))
+            {
+                if (ans[n / 2] < 9)
+                {
+                    ans[n / 2]++;
+                    return ans;
+                }
+                else
+                {
+                    int index = n / 2;
+                    while (index >= 0 && ans[index] == 9)
+                    {
+                        ans[index] = 0;
+                        ans[n - index - 1] = 0;
+                        index--;
+                    }
+                    if (index < 0)
+                    {
+                        ans.insert(ans.begin(), 1);
+                        ans[n] = 1;
+                    }
+                    else
+                    {
+                        ans[index]++;
+                        ans[n - index - 1]++;
+                    }
+                    return ans;
+                }
+            }
+            else
+            {
+
+                if (ans[(n / 2) - 1] < 9)
+                {
+                    ans[n / 2]++;
+                    ans[(n / 2) - 1]++;
+                    return ans;
+                }
+                else
+                {
+                    int index = (n / 2) - 1;
+                    while (index >= 0 && ans[index] == 9)
+                    {
+                        ans[index] = 0;
+                        ans[n - index - 1] = 0;
+                        index--;
+                    }
+                    if (index < 0)
+                    {
+                        ans.insert(ans.begin(), 1);
+                        ans[n] = 1;
+                    }
+                    else
+                    {
+                        ans[index]++;
+                        ans[n - index - 1]++;
+                    }
+                    return ans;
+                }
+            }
+        }
+    }
+
+};
