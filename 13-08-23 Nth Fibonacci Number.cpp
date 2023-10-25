ecfdf4d23e0f3c5e729d8978b791925ee4fb1fcf
Akash Verma <75831162+akashazad-dot@users.noreply.github.com>
2023-10-25 22:23:03 +0530

    13-08-23 Nth Fibonacci Number.cpp

---
+++ 13-08-23 Nth Fibonacci Number.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+class Solution {
+  public:
+   int mod =1000000007.;
+    int nthFibonacci(int n){
+        vector<int>dp(n+1);
+        dp[0] =0;
+        dp[1] =1;
+        for(int i =2;i<=n;i++){
+            dp[i] = (dp[i-1] + dp[i-2])%mod;
+        }
+        return dp[n]%mod;
+    }
+};
