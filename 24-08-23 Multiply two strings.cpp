d2655c0cbaca3f69758f1f75830f29acefa0d283
Akash Verma <75831162+akashazad-dot@users.noreply.github.com>
2023-10-27 11:21:55 +0530

    24-08-23 Multiply two strings.cpp

---
+++ 24-08-23 Multiply two strings.cpp
@@ -0,0 +1,9 @@
+#User function Template for python3
+
+class Solution:
+    def multiplyStrings(self,s1,s2):
+        a = int(s1)
+        b = int(s2)
+        c = a*b
+        ans = str(c)
+        return ans
