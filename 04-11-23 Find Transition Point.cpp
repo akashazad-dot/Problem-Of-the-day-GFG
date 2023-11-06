377c8d6d38a458168d806046f5a2a46e68035bef
Akash Verma <75831162+akashazad-dot@users.noreply.github.com>
2023-11-06 23:45:54 +0530

    04-11-23 Find Transition Point.cpp

---
+++ 04-11-23 Find Transition Point.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    int transitionPoint(int arr[], int n) {
+        for(int i=0; i<n; i++){
+            if(arr[i] != 0){
+                return i;
+                
+            }
+        }
+        return -1;
+    }
+}
